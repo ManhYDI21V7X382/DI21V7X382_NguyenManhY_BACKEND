97c9d5feeeb10c587381e27f651c31cafacedf40
ManhYDI21V7X382 <ydi21v7x382@dttx.ctu.edu.vn>
2023-11-25 00:17:28 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const contactsRouter = require("./routes/contact.route")
-
 const cors = require("cors");
-
-
+const contactsRouter = require("./routes/contact.route")
+const ApiError =require("./api-error");
 
 const app = express();
 
@@ -11,10 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to contact book application."});
+    res.json({message: "Xin chao den ung dung Quan ly danh ba!"});
 });
 
 app.use("/api/contacts", contactsRouter)
 
-module.exports = app;
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+module.exports = app;
